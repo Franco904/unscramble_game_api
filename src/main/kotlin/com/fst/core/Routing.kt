9d64d877718d5b9b60e66361e0fb240a22bb09e4
Franco904 <francostavares2003@gmail.com>
2024-08-05 19:26:03 -0300

    refactor: improve code

--- src/main/kotlin/com/fst/core/Routing.kt
+++ src/main/kotlin/com/fst/core/Routing.kt
@@ -9,15 +9,19 @@ import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     install(Resources)
-    install(StatusPages) {
-        exception<Throwable> { call, cause ->
-            call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
-        }
-    }
+    configureExceptionHandling()
 
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+    }
+}
+
+fun Application.configureExceptionHandling() {
+    install(StatusPages) {
+        exception<Throwable> { call, cause ->
+            call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
         }
     }
 }
